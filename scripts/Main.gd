c961e3c4ccaf440a2d81938137b1d7533545c027
Jack Zhang <jackzhxng@meta.com>
2024-06-16 19:00:24 -0700

    Spawn 5 villagers

---
+++ scripts/Main.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+@export var mob_scene: PackedScene
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var villagers = []
+	for i in range(5):
+		print(i)
+		var villager = mob_scene.instantiate()
+		villager.position = $Player.position
+		villager.position.x += (i + 1) * 50
+		villagers.append(villager)
+		add_child(villager)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
