15c65bb096fc00a7f974ad274d55fa259c7d8ae5
David Zhang <david8zhang@berkeley.edu>
2024-06-19 11:06:07 -0700

    fix villager flee behavior jankiness, add tilemap

--- scripts/fsm/FleeState.gd
+++ scripts/fsm/FleeState.gd
@@ -1,7 +1,7 @@
 extends State
 
 @onready var player = get_node("/root/Main/Player") as Player
-const RUN_SPEED = 250
+const RUN_SPEED = 150
 
 func _physics_process(_delta: float) -> void:
 	var villager = entity as Villager
@@ -9,15 +9,14 @@ func _physics_process(_delta: float) -> void:
 	if dist_to_player > Villager.FLEE_DIST_THRESHOLD:
 		villager._state_machine.transition_to("Idle")
 	else:
-		var angle = rad_to_deg(villager.get_angle_to(player.global_position))
-		var opposite_angle = 0 if dist_to_player < 75 else -angle
-		var dir = Vector2(cos(opposite_angle), sin(opposite_angle))
+		var angle = villager.get_angle_to(player.global_position)
+		var dir = -Vector2.RIGHT.rotated(angle)
 		if player.global_position.x > villager.global_position.x:
 			sprite.flip_h = true
 		else:
 			sprite.flip_h = false
-		
-		villager.translate(dir * _delta * RUN_SPEED)
+
+		villager.position += dir * _delta * RUN_SPEED
 
 func enter(_msg:={}) -> void:
 	sprite.play("run")
