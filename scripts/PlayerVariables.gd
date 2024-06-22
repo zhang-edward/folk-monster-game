39dbaa07c27023036acec3d18b648c5b36bf803d
david8zhang <david8zhang@berkeley.edu>
2024-06-21 20:20:28 -0700

    added power up select menu

--- scripts/PlayerVariables.gd
+++ scripts/PlayerVariables.gd
@@ -1,3 +1,15 @@
 extends Node
 
 var player_score := 0
+var player_power_ups = []
+
+# Power ups
+enum PowerUpTypes {Lunge,AcidSpit,FireBreath,AttackUp,DefenseUp,SpeedUp}
+var player_powerup_levels = {
+	PowerUpTypes.Lunge: 0,
+	PowerUpTypes.AcidSpit: 0,
+	PowerUpTypes.FireBreath: 0,
+	PowerUpTypes.AttackUp: 0,
+	PowerUpTypes.DefenseUp: 0,
+	PowerUpTypes.SpeedUp: 0
+}
--- scripts/PowerUpSelect.gd
+++ scripts/PowerUpSelect.gd
@@ -3,10 +3,60 @@ extends Node2D
 
 @onready var _player_variables := get_node("/root/PlayerVariables") as PlayerVariables
 @onready var score = $CanvasLayer/Control/Score as Label
+@onready var power_up_boxes: Array[PowerUpBox] = [
+	$CanvasLayer/Control/HBoxContainer/PowerUp as PowerUpBox,
+	$CanvasLayer/Control/HBoxContainer/PowerUp2 as PowerUpBox,
+	$CanvasLayer/Control/HBoxContainer/PowerUp3 as PowerUpBox
+]
+
+const POWER_UPS = [
+	{
+		"name": "Attack Up",
+		"type": PlayerVariables.PowerUpTypes.AttackUp,
+		"description": "Increase damage of basic attack by 1",
+		"cost": 2
+	},
+	{
+		"name": "Defense Up",
+		"type": PlayerVariables.PowerUpTypes.DefenseUp,
+		"description": "Decrease incoming damage by 1",
+		"cost": 2
+	},
+	{
+		"name": "Speed Up",
+		"type": PlayerVariables.PowerUpTypes.SpeedUp,
+		"description": "Increase movement speed by 10",
+		"cost": 2
+	},
+	{
+		"name": "AcidSpit",
+		"type": PlayerVariables.PowerUpTypes.AcidSpit,
+		"description": "Launch a fireball forward",
+		"cost": 3
+	},
+	{
+		"name": "Lunge",
+		"type": PlayerVariables.PowerUpTypes.Lunge,
+		"description": "Charge forward rapidly",
+		"cost": 3
+	},
+	{
+		"name": "FireBreath",
+		"type": PlayerVariables.PowerUpTypes.FireBreath,
+		"description": "Breath a cone of fire in front of you",
+		"cost": 3
+	}
+]
 
 func _ready():
-	print(_player_variables.player_score)
 	score.text = "Available Points: " + str(_player_variables.player_score)
+	var random_powerups = POWER_UPS.duplicate()
+	randomize()
+	random_powerups.shuffle()
+	for i in range(0, power_up_boxes.size()):
+		var power_up = random_powerups[i]
+		var power_up_box = power_up_boxes[i]
+		power_up_box.init_powerup(power_up)
 
 func _on_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/main.tscn")
