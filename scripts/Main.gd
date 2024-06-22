1305f7d85a77003debb91f90130d855fbd703b4f
David Zhang <david8zhang@berkeley.edu>
2024-06-21 19:27:28 -0700

    added power up select screen

--- scripts/Main.gd
+++ scripts/Main.gd
@@ -4,7 +4,12 @@ extends Node2D
 @export var num_villagers = 10
 
 @onready var round_timer: Timer = $RoundTimer
+@onready var time_remaining_label = $CanvasLayer/GUI/TimeRemaining as Label
 
+const TIME_LIMIT_SECONDS = 10
+var time_remaining
+
+# Called when the node enters the scene tree for the first time.
 func _ready():
 	# Spawns villagers
 	var villagers = []
@@ -17,3 +22,21 @@ func _ready():
 	
 	# Start the round timer
 	round_timer.start()
+	# Set a time limit for the night
+	time_remaining = TIME_LIMIT_SECONDS
+	time_remaining_label.text = "Time remaining: " + str(time_remaining)
+	var timer = Timer.new()
+	timer.autostart = true
+	timer.wait_time = 1
+	timer.one_shot = false
+	var callable = Callable(self, "on_timer_tick")
+	timer.timeout.connect(callable)
+	add_child(timer)
+
+
+func on_timer_tick():
+	if time_remaining == 0:
+		get_tree().change_scene_to_file("res://scenes/PowerUpSelect.tscn")
+	else:
+		time_remaining -= 1
+		time_remaining_label.text = "Time remaining: " + str(time_remaining)
