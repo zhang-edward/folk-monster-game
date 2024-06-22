dee548818c4335c0ef4a4fca94bf515ba3b2ed43
Edward Zhang <edwardz123@gmail.com>
2024-06-22 02:41:37 -0400

    Add villager sprites

--- scripts/Villager.gd
+++ scripts/Villager.gd
@@ -3,8 +3,14 @@ extends CharacterBody2D
 
 @onready var _player_variables := get_node("/root/PlayerVariables") as PlayerVariables
 @onready var _state_machine: StateMachine = get_node("StateMachine")
+@onready var _sprite: AnimatedSprite2D = $Sprite
 @onready var player: Player = get_node("/root/Main/Player") as Player
 const FLEE_DIST_THRESHOLD = 200
+
+func _ready():
+	var villager_num = randi() % 3 + 1
+	print(villager_num)
+	_sprite.frames = load("res://animations/Villager" + str(villager_num) + ".tres")
 
 func damage():
 	_player_variables.player_score += 1
