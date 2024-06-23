71c6a06fbc42b8a48d1e01b0f037f83b26d9a3ca
Edward Zhang <edwardz123@gmail.com>
2024-06-23 03:21:46 -0400

    Add text for sunrise

--- scripts/Main.gd
+++ scripts/Main.gd
@@ -24,7 +24,11 @@ func _process(delta):
 	player.position.y = clamp(player.position.y, camera.limit_top, camera.limit_bottom)
 
 	time_remaining_label.text = "Time remaining: " + str(snapped(time_remaining, 1))
-	time_remaining -= delta
+	if time_remaining > 0:
+		time_remaining -= delta
+
 	if time_remaining <= 0 and not _switching_scenes:
 		_switching_scenes = true
+		%SceneAnimationPlayer.play("fade_in")
+		await %SceneAnimationPlayer.animation_finished
 		SceneTransition.change_scene_to_file("res://scenes/PowerUpSelect.tscn", 3.0)
