e6958ef6288005cc2a2ec86eb59fec0540d77bf3
david8zhang <david8zhang@berkeley.edu>
2024-06-23 11:57:48 -0700

    display generation number in pre game scene, tweak lunge damage, reset player variables on game over

--- scripts/PreGameScene.gd
+++ scripts/PreGameScene.gd
@@ -5,8 +5,11 @@ var switching = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	var num_generation = _player_variables.generation_number 
+	var text = "Night has fallen...\n\n"
+	text += "[color=red]" + str(num_generation) + get_ordinal_suffix(num_generation) + " generation...[/color]"
 	_player_variables.kill_count = 0
-	%DialogueBox.init("Night has fallen...")
+	%DialogueBox.init("[center]" + text + "[/center]")
 	%DialogueBox.dialogue_finished.connect(_on_DialogueBox_dialogue_finished)
 
 func _on_DialogueBox_dialogue_finished():
@@ -14,3 +17,14 @@ func _on_DialogueBox_dialogue_finished():
 		return
 	switching = true
 	SceneTransition.change_scene_to_file("res://scenes/main.tscn")
+
+func get_ordinal_suffix(num_generation: int):
+	var modded = num_generation % 20
+	if modded == 1:
+		return "st"
+	elif modded == 2:
+		return "nd"
+	elif modded == 3:
+		return "rd"
+	else:
+		return "th"
