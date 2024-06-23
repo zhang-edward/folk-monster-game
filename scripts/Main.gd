ec0a394acd2aa3f0506af7b29555c305b91f20c0
Edward Zhang <edwardz123@gmail.com>
2024-06-23 02:40:40 -0400

    Add health bar

--- scripts/Main.gd
+++ scripts/Main.gd
@@ -6,32 +6,25 @@ class_name Main
 @onready var player: Player = %Player as Player
 @onready var camera: Camera2D = get_node("Player/Camera2D") as Camera2D
 
-
 const TIME_LIMIT_SECONDS = 60
-var time_remaining
+var mob_scene: PackedScene = preload ("res://prefabs/Villager.tscn")
+var guard_scene: PackedScene = preload ("res://prefabs/GuardVillager.tscn")
 
-func _process(_delta):
-	# Prevent the player from going out of bounds of the camera's limits.
-	player.position.x = clamp(player.position.x, camera.limit_left, camera.limit_right)
-	player.position.y = clamp(player.position.y, camera.limit_top, camera.limit_bottom)
+var time_remaining
+var _switching_scenes = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	# Set a time limit for the night
 	time_remaining = player.calculate_attr_with_powerup(PlayerVariables.PowerUpTypes.TimeWarp, TIME_LIMIT_SECONDS, 5)
-	time_remaining_label.text = "Time remaining: " + str(time_remaining)
-	var timer = Timer.new()
-	timer.autostart = true
-	timer.wait_time = 1
-	timer.one_shot = false
-	var callable = Callable(self, "on_timer_tick")
-	timer.timeout.connect(callable)
-	add_child(timer)
 
-func on_timer_tick():
-	if time_remaining == 0:
-		_player_variables.generation_number += 1
-		SceneTransition.change_scene_to_file("res://scenes/PowerUpSelect.tscn")
-	else:
-		time_remaining -= 1
-		time_remaining_label.text = "Time remaining: " + str(time_remaining)
+func _process(delta):
+	# Prevent the player from going out of bounds of the camera's limits.
+	player.position.x = clamp(player.position.x, camera.limit_left, camera.limit_right)
+	player.position.y = clamp(player.position.y, camera.limit_top, camera.limit_bottom)
+
+	time_remaining_label.text = "Time remaining: " + str(snapped(time_remaining, 1))
+	time_remaining -= delta
+	if time_remaining <= 0 and not _switching_scenes:
+		_switching_scenes = true
+		SceneTransition.change_scene_to_file("res://scenes/PowerUpSelect.tscn", 3.0)
