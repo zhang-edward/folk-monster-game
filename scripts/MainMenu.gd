8a534726165fd79c7eb9e1e386321296ea69d52f
Edward Zhang <edwardz123@gmail.com>
2024-06-23 14:48:37 -0400

    Add tutorial

--- scripts/MainMenu.gd
+++ scripts/MainMenu.gd
@@ -1,12 +1,21 @@
 extends Node2D
 
+@onready var main_menu = %Menu
+@onready var tutorial = %Tutorial
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	%PlayButton.pressed.connect(_on_PlayButton_pressed)
+	%TutorialButton.pressed.connect(_on_TutorialButton_pressed)
+	%BackButton.pressed.connect(_on_BackButton_pressed)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _on_TutorialButton_pressed():
+	tutorial.show()
+	main_menu.hide()
+
+func _on_BackButton_pressed():
+	tutorial.hide()
+	main_menu.show()
 
 func _on_PlayButton_pressed():
 	%AnimationPlayer.play("Start")
