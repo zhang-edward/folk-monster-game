417d0e8df39a2993922c5a46190abece01dc71c1
Edward Zhang <edwardz123@gmail.com>
2024-06-22 22:06:36 -0400

    Add sunrise

---
+++ scripts/DayNight.gd
@@ -0,0 +1,16 @@
+extends CanvasModulate
+
+var START_COLOR = Color.hex(0x414098FF)
+var END_COLOR = Color.hex(0xD98027FF)
+
+var main_node: Main
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	main_node = get_parent()
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var t = (Main.TIME_LIMIT_SECONDS - main_node.time_remaining) / Main.TIME_LIMIT_SECONDS
+	color = START_COLOR.lerp(END_COLOR, pow(t, 4))
--- scripts/Main.gd
+++ scripts/Main.gd
@@ -1,4 +1,5 @@
 extends Node2D
+class_name Main
 
 @export var mob_scene: PackedScene
 @export var num_villagers = 50
